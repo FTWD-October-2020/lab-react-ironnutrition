5bb997da1053b1c502f861835092b062afb80e1c
aparr075 <aparr075@fiu.edu>
2020-11-18 16:10:55 -0500

    ana

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
 import './App.css';
+import FoodBox from './FoodBox'
 
 function App() {
+  const FoodBoxes = (food) => {
+    return foods.map(eachFood => {
+      return <FoodBox {...eachFood} />
+    })
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <FoodBoxes />
+  )
 }
 
 export default App;
+
+
